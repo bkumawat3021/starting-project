ca8e02236847d69e29565c3fba51f8ff978dbef5
bkumawat3021 <143257248+bkumawat3021@users.noreply.github.com>
2023-08-26 02:23:34 -0700

    Add files via upload

---
+++ bhavdesh/js.js
@@ -0,0 +1,10 @@
+
+var a, b ,c, d, e;
+
+a=parseInt(prompt("entar value of a"))
+b=parseInt(prompt("entar value of b"))
+c=parseInt(prompt("entar value of c"))
+d=parseInt(prompt("entar value of d"))
+
+e=a+b+c+d
+document.write(e)
